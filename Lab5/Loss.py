9ce7056fa5562a00451eed5965d58f624f093903
liuutin9 <liuutin9@gmail.com>
2024-11-18 10:10:54 +0800

    update

---
+++ Lab5/Loss.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def compute_BCE_loss(AL, Y):
+    """
+    Implement the binary cross-entropy loss function using the above formula.
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (n, 1)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (n, 1)
+
+    Returns:
+    loss -- binary cross-entropy loss
+    """
+
+    n = Y.shape[0]
+
+    # Compute loss from aL and y.
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = -(np.sum(Y * np.log(AL + 1e-5) + (1 - Y) * np.log(1 - AL + 1e-5))) / n
+    ### END CODE HERE ###
+
+    loss = np.squeeze(loss)      # To make sure your loss's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(loss.shape == ())
+
+    return loss
